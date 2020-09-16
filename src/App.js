86b0f705711cf628e1fabd8f821926ed30b79e80
yeremiaChris <yeremia997@gmail.com>
2020-09-16 16:45:12 +0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// Route artinya renders out the compoenents based on the url
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // we have ability to use routing
+    <Router>
+      <div className="App">
+        {/* switch berfungsi untuk memberhentikan setiap route jika ketemu satu dan sesuai dengan url akan di berhentikan dan akan merender component tersebut yang match */}
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+        {/* <About /> */}
+        {/* <Shop /> */}
+      </div>
+    </Router>
   );
 }
+
+// const Home = () => {
+//   <div>
+//     <h1>home</h1>
+//   </div>;
+// };
 
 export default App;
